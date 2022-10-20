32110034e38e8d982e6b25023b265acf6b588374
polvinh0 <ieiel_4@hotmail.com>
2022-10-20 14:59:51 -0300

    aulas

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProdutoCardComponent } from './produto-card/produto-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoCardComponent
   ],
   imports: [
     BrowserModule
---
+++ diretivas/src/app/produto-card/produto-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'produto-card',
+  templateUrl: './produto-card.component.html',
+  styleUrls: ['./produto-card.component.css']
+})
+export class ProdutoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
